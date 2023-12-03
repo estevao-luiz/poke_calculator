7854bd404141ce11ef9e6d003d27eff6f9c06816
Estevão Luiz Cabral Lopes <126028553+estevao-luiz@users.noreply.github.com>
2023-12-03 00:12:06 -0300

    Update exe.py
    A única entrada na janela interativa desse código é o conteúdo da varável "party".

--- exe.py
+++ exe.py
@@ -7,7 +7,7 @@ import interactive_map as mp
 #province = "" 
 #area = "" 
 #biome = "" 
-#party = ""
+#party = []
 
 class PokemonApp(Tk):
         def __init__(self):
@@ -15,11 +15,15 @@ class PokemonApp(Tk):
             self.title("Pokemon App")
             self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}")
            
-            # Adicione um atributo de instância para armazenar a province
-            self.province = mp.province_value
-            print(self.province)
+            #Adicione um atributo de instância para armazenar a province
+            self.version  = mp.version_value 
+            self.province = mp.province_value 
+            self.area = mp.area_value 
+            self.biome  = mp.biome_value
             
-            # Criando os frames
+            print(self.version, self.province, self.area, self.biome)
+            
+            #Criando os frames
             #self.column1 = Frame(self)
             #self.column1.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
     
@@ -27,17 +31,17 @@ class PokemonApp(Tk):
             #self.column2.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
     
             # Labels e Entries na coluna 1
-            labels_column = ["Escolha a versão da Pokedex:", "Escolha a área:", "Escolha o bioma:"]
-            for row, label_text in enumerate(labels_column):
-                label = Label(self, text=label_text)
-                label.grid(row=row, column=0, sticky="w", pady=5)
-                entry = Entry(self)
-                entry.grid(row=row, column=1, pady=5)
+            #labels_column = ["Escolha a versão da Pokedex:", "Escolha a área:", "Escolha o bioma:"]
+            #for row, label_text in enumerate(labels_column):
+            #   label = Label(self, text=label_text)
+            #   label.grid(row=row, column=0, sticky="w", pady=5)
+            #   entry = Entry(self)
+            #   entry.grid(row=row, column=1, pady=5)
             
             #Adiciona coluna vazia    
             self.grid_columnconfigure(2, weight=1)
     
-            # Labels e Entries na coluna 2
+            #Labels e Entries na coluna 2
             label_team = Label(self, text="Equipe de Pokémon:")
             label_team.grid(row=0, column=3, sticky="w", pady=5)
     
@@ -57,22 +61,22 @@ class PokemonApp(Tk):
             
         def show_team(self, pokemon_entries):
                 
-                version_entry = self.grid_slaves(row=0, column=1)  # Acessa o primeiro widget da lista
-                version = str(version_entry[0].get())
+                #version_entry = self.grid_slaves(row=0, column=1)  # Acessa o primeiro widget da lista
+                #version = str(version_entry[0].get())
                 
-                area_entry = self.grid_slaves(row=1, column=1)
-                area = str(area_entry[0].get())
+                #area_entry = self.grid_slaves(row=1, column=1)
+                #area = str(area_entry[0].get())
                 
-                biome_entry = self.grid_slaves(row=2, column=1)
-                biome = (biome_entry[0].get())
+                #biome_entry = self.grid_slaves(row=2, column=1)
+                #biome = (biome_entry[0].get())
                 
                 team = [entry.get() if entry else "" for entry in pokemon_entries] + [""]*(6-len(pokemon_entries))
                 
                 Tk.destroy(self)
                 
-                result = main(version, self.province, area, biome, team)  # Captura o resultado e a mensagem
+                result = main(self.version, self.province, self.area, self.biome, team)  # Captura o resultado e a mensagem
                         
-                # Exibe a mensagem com o resultado
+                #Exibe a mensagem com o resultado
                 messagebox.showinfo("Uma equipe play destroy é:", f"{result}")                       
             
 def main(version, province, area, biome, party):
